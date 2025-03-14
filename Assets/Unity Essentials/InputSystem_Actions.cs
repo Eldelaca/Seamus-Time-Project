2a1f5c952d70dffee07f7e5f9df4b86740ca5bc5
kb478 <c23789389@mytudublin.ie>
2025-03-14 00:56:10 +0000

    Fixed?

--- Assets/Unity Essentials/InputSystem_Actions.cs
+++ Assets/Unity Essentials/InputSystem_Actions.cs
@@ -155,30 +155,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""initialStateCheck"": false
                 },
                 {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-                    ""name"": ""Pause"",
-                    ""type"": ""Button"",
-                    ""id"": ""ae99f71b-c21a-4c13-b8d3-8478410594eb"",
-                    ""expectedControlType"": """",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
-                },
-                {
-                    ""name"": ""Enter_Menu"",
-                    ""type"": ""Button"",
-                    ""id"": ""cb9c5a13-7156-4c3c-95ec-523fc1dbc9b7"",
-=======
                     ""name"": ""Input"",
                     ""type"": ""Button"",
-                    ""id"": ""201a2d41-217b-40b8-a971-906a981f141e"",
->>>>>>> Stashed changes
-=======
-                    ""name"": ""Input"",
-                    ""type"": ""Button"",
-                    ""id"": ""201a2d41-217b-40b8-a971-906a981f141e"",
->>>>>>> Stashed changes
+                    ""id"": ""8c580da6-ece7-44bc-9a84-656a6e27079e"",
                     ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
@@ -551,38 +530,12 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": """",
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-                    ""id"": ""f8d8fe62-21b1-4467-98b2-b627c5225625"",
-                    ""path"": ""<Keyboard>/escape"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Pause"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""e37230f2-d573-4ca8-be51-b934a12152c4"",
-                    ""path"": ""<Keyboard>/escape"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Enter_Menu"",
-=======
-=======
->>>>>>> Stashed changes
-                    ""id"": ""9e2af029-696a-484e-8486-729fa367f532"",
+                    ""id"": ""af39d79d-2a13-4efc-9dc6-f5a88e6962da"",
                     ""path"": ""<Keyboard>/e"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": "";Keyboard&Mouse"",
                     ""action"": ""Input"",
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -678,15 +631,6 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""type"": ""PassThrough"",
                     ""id"": ""9caa3d8a-6b2f-4e8e-8bad-6ede561bd9be"",
                     ""expectedControlType"": ""Quaternion"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
-                },
-                {
-                    ""name"": ""Exit_Menu"",
-                    ""type"": ""Button"",
-                    ""id"": ""8c435ba8-a0f5-45d8-b9b6-83d0282815bc"",
-                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -1110,17 +1054,6 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""action"": ""TrackedDeviceOrientation"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""61785a98-0269-48fb-a741-b6f96f631cc8"",
-                    ""path"": ""<Keyboard>/escape"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Exit_Menu"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -1197,16 +1130,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         m_Player_Crouch = m_Player.FindAction("Crouch", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        m_Player_Pause = m_Player.FindAction("Pause", throwIfNotFound: true);
-        m_Player_Enter_Menu = m_Player.FindAction("Enter_Menu", throwIfNotFound: true);
-=======
         m_Player_Input = m_Player.FindAction("Input", throwIfNotFound: true);
->>>>>>> Stashed changes
-=======
-        m_Player_Input = m_Player.FindAction("Input", throwIfNotFound: true);
->>>>>>> Stashed changes
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1219,7 +1143,6 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         m_UI_ScrollWheel = m_UI.FindAction("ScrollWheel", throwIfNotFound: true);
         m_UI_TrackedDevicePosition = m_UI.FindAction("TrackedDevicePosition", throwIfNotFound: true);
         m_UI_TrackedDeviceOrientation = m_UI.FindAction("TrackedDeviceOrientation", throwIfNotFound: true);
-        m_UI_Exit_Menu = m_UI.FindAction("Exit_Menu", throwIfNotFound: true);
     }
 
     ~@InputSystem_Actions()
@@ -1308,16 +1231,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Crouch;
     private readonly InputAction m_Player_Jump;
     private readonly InputAction m_Player_Sprint;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    private readonly InputAction m_Player_Pause;
-    private readonly InputAction m_Player_Enter_Menu;
-=======
     private readonly InputAction m_Player_Input;
->>>>>>> Stashed changes
-=======
-    private readonly InputAction m_Player_Input;
->>>>>>> Stashed changes
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -1358,25 +1272,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// </summary>
         public InputAction @Sprint => m_Wrapper.m_Player_Sprint;
         /// <summary>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        /// Provides access to the underlying input action "Player/Pause".
-        /// </summary>
-        public InputAction @Pause => m_Wrapper.m_Player_Pause;
-        /// <summary>
-        /// Provides access to the underlying input action "Player/Enter_Menu".
-        /// </summary>
-        public InputAction @Enter_Menu => m_Wrapper.m_Player_Enter_Menu;
-=======
         /// Provides access to the underlying input action "Player/Input".
         /// </summary>
         public InputAction @Input => m_Wrapper.m_Player_Input;
->>>>>>> Stashed changes
-=======
-        /// Provides access to the underlying input action "Player/Input".
-        /// </summary>
-        public InputAction @Input => m_Wrapper.m_Player_Input;
->>>>>>> Stashed changes
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -1424,24 +1322,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Sprint.started += instance.OnSprint;
             @Sprint.performed += instance.OnSprint;
             @Sprint.canceled += instance.OnSprint;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-            @Pause.started += instance.OnPause;
-            @Pause.performed += instance.OnPause;
-            @Pause.canceled += instance.OnPause;
-            @Enter_Menu.started += instance.OnEnter_Menu;
-            @Enter_Menu.performed += instance.OnEnter_Menu;
-            @Enter_Menu.canceled += instance.OnEnter_Menu;
-=======
             @Input.started += instance.OnInput;
             @Input.performed += instance.OnInput;
             @Input.canceled += instance.OnInput;
->>>>>>> Stashed changes
-=======
-            @Input.started += instance.OnInput;
-            @Input.performed += instance.OnInput;
-            @Input.canceled += instance.OnInput;
->>>>>>> Stashed changes
         }
 
         /// <summary>
@@ -1474,24 +1357,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Sprint.started -= instance.OnSprint;
             @Sprint.performed -= instance.OnSprint;
             @Sprint.canceled -= instance.OnSprint;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-            @Pause.started -= instance.OnPause;
-            @Pause.performed -= instance.OnPause;
-            @Pause.canceled -= instance.OnPause;
-            @Enter_Menu.started -= instance.OnEnter_Menu;
-            @Enter_Menu.performed -= instance.OnEnter_Menu;
-            @Enter_Menu.canceled -= instance.OnEnter_Menu;
-=======
             @Input.started -= instance.OnInput;
             @Input.performed -= instance.OnInput;
             @Input.canceled -= instance.OnInput;
->>>>>>> Stashed changes
-=======
-            @Input.started -= instance.OnInput;
-            @Input.performed -= instance.OnInput;
-            @Input.canceled -= instance.OnInput;
->>>>>>> Stashed changes
         }
 
         /// <summary>
@@ -1539,7 +1407,6 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
     private readonly InputAction m_UI_ScrollWheel;
     private readonly InputAction m_UI_TrackedDevicePosition;
     private readonly InputAction m_UI_TrackedDeviceOrientation;
-    private readonly InputAction m_UI_Exit_Menu;
     /// <summary>
     /// Provides access to input actions defined in input action map "UI".
     /// </summary>
@@ -1591,10 +1458,6 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "UI/TrackedDeviceOrientation".
         /// </summary>
         public InputAction @TrackedDeviceOrientation => m_Wrapper.m_UI_TrackedDeviceOrientation;
-        /// <summary>
-        /// Provides access to the underlying input action "UI/Exit_Menu".
-        /// </summary>
-        public InputAction @Exit_Menu => m_Wrapper.m_UI_Exit_Menu;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -1651,9 +1514,6 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @TrackedDeviceOrientation.started += instance.OnTrackedDeviceOrientation;
             @TrackedDeviceOrientation.performed += instance.OnTrackedDeviceOrientation;
             @TrackedDeviceOrientation.canceled += instance.OnTrackedDeviceOrientation;
-            @Exit_Menu.started += instance.OnExit_Menu;
-            @Exit_Menu.performed += instance.OnExit_Menu;
-            @Exit_Menu.canceled += instance.OnExit_Menu;
         }
 
         /// <summary>
@@ -1695,9 +1555,6 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @TrackedDeviceOrientation.started -= instance.OnTrackedDeviceOrientation;
             @TrackedDeviceOrientation.performed -= instance.OnTrackedDeviceOrientation;
             @TrackedDeviceOrientation.canceled -= instance.OnTrackedDeviceOrientation;
-            @Exit_Menu.started -= instance.OnExit_Menu;
-            @Exit_Menu.performed -= instance.OnExit_Menu;
-            @Exit_Menu.canceled -= instance.OnExit_Menu;
         }
 
         /// <summary>
@@ -1853,35 +1710,12 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnSprint(InputAction.CallbackContext context);
         /// <summary>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        /// Method invoked when associated input action "Pause" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
-=======
         /// Method invoked when associated input action "Input" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
->>>>>>> Stashed changes
-=======
-        /// Method invoked when associated input action "Input" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
->>>>>>> Stashed changes
         /// </summary>
         /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        void OnPause(InputAction.CallbackContext context);
-        /// <summary>
-        /// Method invoked when associated input action "Enter_Menu" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
-        /// </summary>
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
-        void OnEnter_Menu(InputAction.CallbackContext context);
-=======
         void OnInput(InputAction.CallbackContext context);
->>>>>>> Stashed changes
-=======
-        void OnInput(InputAction.CallbackContext context);
->>>>>>> Stashed changes
     }
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "UI" which allows adding and removing callbacks.
@@ -1960,12 +1794,5 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnTrackedDeviceOrientation(InputAction.CallbackContext context);
-        /// <summary>
-        /// Method invoked when associated input action "Exit_Menu" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
-        /// </summary>
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
-        void OnExit_Menu(InputAction.CallbackContext context);
     }
 }
